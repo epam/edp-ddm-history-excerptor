c1c37fc8b0a2fe2d35493c79941a496076ddf122
Yevhen Zvarych <yevhen_zvarych@epam.com>
2021-08-25 14:42:51 +0000

    [EDP-DDM-8091] - First commit

---
+++ src/main/java/com/epam/digital/data/platform/history/HistoryExcerptorApplication.java
@@ -0,0 +1,12 @@
+package com.epam.digital.data.platform.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryExcerptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryExcerptorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/epam/digital/data/platform/history/HistoryExcerptorApplicationTest.java
@@ -0,0 +1,13 @@
+package com.epam.digital.data.platform.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryExcerptorApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
